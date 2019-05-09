3da125ced0e25d978a4e36797ca4b912992c0bd3
Lucas Gontijo <31103149+Lucasgontijo0@users.noreply.github.com>
2019-05-09 17:26:07 -0300

    Update build-stub.sh

--- build-stub.sh
+++ build-stub.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 FILE=/usr/local/stubby.yml
-DIRECTORY=/usr/local/stub
+DIRECTORY=/usr/local/stub/
 
 createStubFile(){
-    find ${DIRECTORY} -name "*.yml" -print | xargs cat >> ${FILE}
+    find ${DIRECTORY} -name "*.yml" | xargs cat > ${FILE}
 }
 
 createStubFile
